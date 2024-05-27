c509415d7a441c1456eeaefece25444e295869d6
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-05-28 02:15:07 +0900

    feat: Header 기본 UI 작성

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,37 @@
+import "./Header.css";
+import { ChangeEvent, useState } from "react";
+
+const MenuItem = [{ name: "menu1" }, { name: "menu2" }, { name: "menu3" }];
 const Header = () => {
-  return <header>Header</header>;
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">Logo</div>
+
+      <div className="right-section">
+        <div className="menu-box">
+          {MenuItem.map((menu) => (
+            <button key={menu.name} className="menu-button">
+              {menu.name}
+            </button>
+          ))}
+        </div>
+        <div>
+          <input
+            className="search-input"
+            value={searchValue}
+            onChange={handleSearchFieldChange}
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
