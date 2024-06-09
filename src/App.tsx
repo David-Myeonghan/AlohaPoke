7186fa1b26d659caeec922394116c2cec09fbb7b
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-06-09 23:08:05 +0900

    feat: react-query Provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./utils/routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
