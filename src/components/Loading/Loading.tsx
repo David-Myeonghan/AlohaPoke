be53877e12ecb2c92bf81c824ec0021af0dab85b
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-06-11 02:10:31 +0900

    feat: 로딩 컴포넌트화

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,14 @@
 import styles from "./Loading.module.scss";
 import classNames from "classnames/bind";
+import React from "react";
+
+type LoadingPropType = {
+  size?: "small" | "medium";
+};
 
 const cx = classNames.bind(styles);
-export default function Loading() {
-  return <div className={cx("spinner")}></div>;
+export default function Loading({ size = "medium" }: LoadingPropType) {
+  const className = cx(size, "spinner");
+
+  return <div className={className}></div>;
 }
