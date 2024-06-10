494184ef7d8408bbb9f312ae8f05f90611059f0b
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-06-11 02:20:10 +0900

    feat: 이미지 개별 로딩 도입

--- src/pages/MainList/ui/PokemonCard.tsx
+++ src/pages/MainList/ui/PokemonCard.tsx
@@ -2,6 +2,8 @@ import { PokemonResponseType } from "utils/queries";
 import styles from "./PokemonCard.module.scss";
 import classNames from "classnames/bind";
 import { Typography } from "components";
+import { useEffect, useRef } from "react";
+import Loading from "components/Loading/Loading";
 
 const cx = classNames.bind(styles);
 
@@ -9,9 +11,43 @@ type PokemonCardPropType = {
   pokemon: PokemonResponseType;
 };
 export default function PokemonCard({ pokemon }: PokemonCardPropType) {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const loadingRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+    const loadingElement = loadingRef.current;
+
+    const showImage = () => {
+      if (loadingElement) {
+        loadingElement.style.display = "none";
+      }
+      if (imageElement) {
+        imageElement.style.display = "block";
+      }
+    };
+
+    if (imageElement) {
+      imageElement.addEventListener("load", showImage);
+    }
+
+    return () => {
+      if (imageElement) {
+        imageElement.removeEventListener("load", showImage);
+      }
+    };
+  }, []);
+
   return (
     <div className={cx("Card-layout")}>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <div ref={loadingRef} className={cx("Loading-box")}>
+        <Loading size="small" />
+      </div>
+      <img
+        src={pokemon.sprites.front_default}
+        alt={pokemon.name}
+        ref={imageRef}
+      />
       <Typography size={"t3"}>{pokemon.name}</Typography>
     </div>
   );
