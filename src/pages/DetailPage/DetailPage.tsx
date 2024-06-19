c67897e5aa9f1e8479c23bda5a54951272124a69
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-06-20 01:36:14 +0900

    feat: URLSearchParms 를 이용해서 디테일 페이지 이동

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,11 +1,16 @@
 import styles from "./DetailPage.module.scss";
 import classNames from "classnames/bind";
+import { useLocation } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 export default function DetailPage() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const name = queryParams.get("name");
+
   return (
     <div>
-      <span>Detail Page 1223</span>
+      <span>{name}</span>
     </div>
   );
 }
