9ad6e6887344357e29a16fb85af57ec2c12300c5
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-06-20 02:12:02 +0900

    feat: 포켓몬 나타낼 속성 정리

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,6 +1,8 @@
 import styles from "./DetailPage.module.scss";
 import classNames from "classnames/bind";
 import { useLocation } from "react-router-dom";
+import { usePokemonDetail } from "../../hooks/usePokemonDetail";
+import { useEffect } from "react";
 
 const cx = classNames.bind(styles);
 export default function DetailPage() {
@@ -8,9 +10,27 @@ export default function DetailPage() {
   const queryParams = new URLSearchParams(location.search);
   const name = queryParams.get("name");
 
+  const { data } = usePokemonDetail(name ?? "");
+  console.log(data);
+
+  if (!data) return <div>Error</div>;
+
   return (
     <div>
-      <span>{name}</span>
+      <div>{data?.name}</div>
+      <div>{data?.id}</div>
+      <div>{data?.base_experience}</div>
+      <div>{data?.height}</div>
+      <div>{data?.weight}</div>
+      {data?.types.map(({ type }) => <div key={type.name}>{type.name}</div>)}
+      {data?.stats.map(({ base_stat, stat }) => (
+        <div key={stat.name}>
+          {stat.name}: {base_stat}
+        </div>
+      ))}
+      {data?.abilities.map(({ ability }) => (
+        <div key={ability.name}>{ability.name}</div>
+      ))}
     </div>
   );
 }
--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -1,6 +1,29 @@
 export type PokemonDetailResponseType = {
-  sprites: { front_default: string };
+  sprites: {
+    front_default: string;
+    other: {
+      dream_world: { front_default: string };
+      "official-artwork": { front_default: string };
+    };
+  };
   name: string;
+  id: number;
+  base_experience: number;
+  height: number;
+  weight: number;
+  //
+  types: { slot: number; type: { name: string; url: string } }[];
+  //
+  stats: {
+    base_stat: number;
+    effort: number;
+    stat: { name: string; url: string };
+  }[];
+  abilities: {
+    ability: { name: string; url: string };
+    is_hidden: boolean;
+    slot: number;
+  }[];
 };
 
 export type pokemonType = {
