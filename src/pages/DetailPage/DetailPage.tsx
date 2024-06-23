a3ca15d7d57297196bfa48fba8a12edcf8bcc629
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-06-23 15:21:16 +0900

    fix: data 조건 해제

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -5,24 +5,27 @@ import { usePokemonDetail } from "../../hooks/usePokemonDetail";
 import { Button, Loading, Typography } from "../../components";
 import PokemonImages from "./ui/PokemonImages";
 import PokemonStats from "./ui/PokemonStats";
+import { ErrorPage } from "../ErrorPage";
 
 const cx = classNames.bind(styles);
 export default function DetailPage() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const name = queryParams.get("name");
+  const paramName = queryParams.get("name");
 
   const navigate = useNavigate();
 
-  const { data, isLoading } = usePokemonDetail(name ?? "");
+  const { data, isLoading } = usePokemonDetail(paramName ?? "");
 
-  if (data == null) return null;
   if (isLoading)
     return (
       <div className={cx("loading")}>
         <Loading />
       </div>
     );
+  if (data == null) {
+    return <ErrorPage />;
+  }
 
   return (
     <div className={cx("container")}>
@@ -37,20 +40,20 @@ export default function DetailPage() {
 
       <div className={cx("intro-section")}>
         <div className={cx("name-box")}>
-          <Typography size={"t1"}>{data?.name}</Typography>
-          <Typography size={"t3"}>#&nbsp;{data?.id}</Typography>
+          <Typography size={"t1"}>{data.name}</Typography>
+          <Typography size={"t3"}>#&nbsp;{data.id}</Typography>
         </div>
       </div>
 
       <div className={cx("character-section")}>
         <div>
           <Typography size={"t2"}>
-            {`Height: ${data?.height} cm`}&nbsp;
+            {`Height: ${data.height} cm`}&nbsp;
           </Typography>
-          <Typography size={"t2"}>{`Weight: ${data?.weight} kg`}</Typography>
+          <Typography size={"t2"}>{`Weight: ${data.weight} kg`}</Typography>
         </div>
 
-        {data?.types.map(({ type, slot }) => (
+        {data.types.map(({ type, slot }) => (
           <div key={type.name}>
             <Typography size={"t3"}>{type.name}</Typography>
           </div>
