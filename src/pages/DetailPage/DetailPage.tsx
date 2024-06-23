6daa93ab0b0cbf408193c7e2e1201e2a36a34774
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-06-23 18:14:12 +0900

    fix: data = null 일 경우 제거

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -5,7 +5,6 @@ import { usePokemonDetail } from "../../hooks/usePokemonDetail";
 import { Button, Loading, Typography } from "../../components";
 import PokemonImages from "./ui/PokemonImages";
 import PokemonStats from "./ui/PokemonStats";
-import { ErrorPage } from "../ErrorPage";
 
 const cx = classNames.bind(styles);
 export default function DetailPage() {
@@ -23,9 +22,6 @@ export default function DetailPage() {
         <Loading />
       </div>
     );
-  if (data == null) {
-    return <ErrorPage />;
-  }
 
   return (
     <div className={cx("container")}>
@@ -36,31 +32,31 @@ export default function DetailPage() {
         </Button>
       </div>
 
-      <PokemonImages sprites={data.sprites} />
+      {data?.sprites != null ? <PokemonImages sprites={data.sprites} /> : null}
 
       <div className={cx("intro-section")}>
         <div className={cx("name-box")}>
-          <Typography size={"t1"}>{data.name}</Typography>
-          <Typography size={"t3"}>#&nbsp;{data.id}</Typography>
+          <Typography size={"t1"}>{data?.name}</Typography>
+          <Typography size={"t3"}>#&nbsp;{data?.id}</Typography>
         </div>
       </div>
 
       <div className={cx("character-section")}>
         <div>
           <Typography size={"t2"}>
-            {`Height: ${data.height} cm`}&nbsp;
+            {`Height: ${data?.height} cm`}&nbsp;
           </Typography>
-          <Typography size={"t2"}>{`Weight: ${data.weight} kg`}</Typography>
+          <Typography size={"t2"}>{`Weight: ${data?.weight} kg`}</Typography>
         </div>
 
-        {data.types.map(({ type, slot }) => (
+        {data?.types.map(({ type, slot }) => (
           <div key={type.name}>
             <Typography size={"t3"}>{type.name}</Typography>
           </div>
         ))}
       </div>
 
-      <PokemonStats stats={data.stats} />
+      {data?.stats != null ? <PokemonStats stats={data.stats} /> : null}
     </div>
   );
 }
