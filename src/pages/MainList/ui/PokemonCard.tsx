719143a494c7e6dd9e480097c8913dea2cf066eb
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-07-11 00:36:05 +0900

    feat: LazyLoadImage 컴포넌트 적용

--- src/pages/MainList/ui/PokemonCard.tsx
+++ src/pages/MainList/ui/PokemonCard.tsx
@@ -1,7 +1,6 @@
 import styles from "./PokemonCard.module.scss";
 import classNames from "classnames/bind";
-import { Typography, Loading } from "components";
-import { useEffect, useRef } from "react";
+import { Typography, LazyLoadImage } from "components";
 import { pokemonType } from "types/pokemon";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "constants/routers";
@@ -15,46 +14,14 @@ export default function PokemonCard({ pokemon }: PokemonCardPropType) {
   const navigate = useNavigate();
   const pokemonId = pokemon.url.match(/(?<=\b\/)\d+/)?.["0"];
 
-  const loadingRef = useRef<HTMLDivElement>(null);
-  const imageRef = useRef<HTMLImageElement>(null);
-
-  useEffect(() => {
-    const loadingElement = loadingRef.current;
-    const imageElement = imageRef.current;
-    const handleLoad = () => {
-      if (loadingElement) {
-        loadingElement.style.display = "none";
-      }
-      if (imageElement) {
-        imageElement.style.visibility = "visible";
-      }
-    };
-
-    if (imageElement) {
-      imageElement.onload = handleLoad;
-    }
-
-    return () => {
-      if (imageElement) {
-        imageElement.onload = null;
-      }
-    };
-  }, []);
-
   return (
     <div
       className={cx("card-layout")}
       onClick={() => navigate(`${ROUTES.detail.root}?name=${pokemon.name}`)}
     >
-      <div ref={loadingRef} className={cx("loading-box")}>
-        <Loading size="small" />
-      </div>
-
-      <img
-        src={`https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
+      <LazyLoadImage
+        imageSource={`https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
         alt={pokemon.name}
-        loading="lazy"
-        ref={imageRef}
       />
       <Typography size={"t3"}>{pokemon.name}</Typography>
     </div>
